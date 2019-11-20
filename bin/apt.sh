9260a2801c64faf10d738a353afd9bfc7cc72108
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-11-20 12:51:57 +0000

    ⚡apt: adding curl/wget installs for wsl

--- bin/apt.sh
+++ bin/apt.sh
@@ -2,5 +2,10 @@
 sudo apt update
 sudo apt upgrade -y
 
+# we don't install git because to get the repo you'd need it
+
+# curl and wget (wsl likes to not have basic stuff)
+sudo apt install curl wget -y
+
 # zsh
 . ~/.dotfiles/zsh/bin/apt.sh
