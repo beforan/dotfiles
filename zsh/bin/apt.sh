d26b3044d0ab3f950da80c5bf890e44fab577012
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-11-20 13:01:53 +0000

    🐛zsh: should fix p10k install

--- zsh/bin/apt.sh
+++ zsh/bin/apt.sh
@@ -1,14 +1,26 @@
+##
 # zsh
+##
 sudo apt install zsh -y
 
-# oh my zsh (unattended; does not set default shell)
+##
+# oh my zsh
+##
+
+# (unattended; does not set default shell)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
+# load zsh at this point so p10k will install correctly
+zsh
+
+# chuck omz's config and link ours
 cd ~
 rm .zshrc
 ln -s ./.dotfiles/zsh/.zshrc .zshrc
 
+##
 # powerline10k
+##
 
 # link the config first so it doesn't run interactive configuration
 ln -s ./.dotfiles/zsh/.p10k.zsh .p10k.zsh
