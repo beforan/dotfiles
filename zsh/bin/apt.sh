db15304af0d6e0872141968942ac5253f16ee2ce
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-11-20 12:57:41 +0000

    🚧zsh: oh-my-zsh rudely overwrites configs

--- zsh/bin/apt.sh
+++ zsh/bin/apt.sh
@@ -1,13 +1,16 @@
 # zsh
 sudo apt install zsh -y
 
-# now let's pre-link the configs so the following don't run any interactive config
-cd ~
-ln -s ./.dotfiles/zsh/.zshrc .zshrc
-ln -s ./.dotfiles/zsh/.p10k.zsh .p10k.zsh
-
 # oh my zsh (unattended; does not set default shell)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
+cd ~
+rm .zshrc
+ln -s ./.dotfiles/zsh/.zshrc .zshrc
+
 # powerline10k
+
+# link the config first so it doesn't run interactive configuration
+ln -s ./.dotfiles/zsh/.p10k.zsh .p10k.zsh
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
