a9ae487daf0348d6f29afbe0d88c325b209d81e1
Jonathan Couldridge <jcouldridge@outlook.com>
2024-05-30 21:26:04 +0100

    initial home manager files for linux

---
+++ home.nix
@@ -0,0 +1,113 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  nixpkgs = {
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "beforan";
+  home.homeDirectory = "/home/beforan";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # base system stuff
+
+    # viable stuff currently layered in rpm-ostree:
+    # git # would be nice to config via programs.git.enable as well
+    # nodejs
+    # python? # in many distros it would be there by default like bash - not sure about nixos. we can add per-project versions via nix or venv too
+    # btop
+    # zsh # programs.zsh.enable would be ideal, and remove hm-session-vars from .zshrc # TODO manage config from here!
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    (nerdfonts.override {fonts = ["FantasqueSansMono" "CascadiaCode"];})
+
+    # warp-terminal # this might be nice once the package matures
+    bat
+    eza
+    cheat
+    atuin
+    starship # TODO manage config from here!
+
+    # "global" dev environment stuff
+    # tools
+    alejandra
+    dotnet-sdk_8
+
+    # apps
+    vscode
+    gitkraken # TODO may not need if lazygit is awesome
+    lazygit
+    jetbrains.rider
+    github-cli
+    # jetbrains.datagrip
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/beforan/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+    VISUAL = "code";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
