d18a7b1f06579f0473cddb39c4f9ed8fb41acef6
Jonathan Couldridge <jcouldridge@outlook.com>
2024-07-04 13:44:05 +0100

    updates

--- nixfiles/auron/configuration.nix
+++ nixfiles/auron/configuration.nix
@@ -112,8 +112,9 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    git
     wget
+
+    git
     fastfetch
     kdePackages.discover
     ptyxis
